b3b858b96666b07d45f49ee0581ae97f103935e0
TRADELUXEFX <tradeluxefx@gmail.com>
2026-08-29 05:52:02 +0100

    Update GroupsActivity.kt

--- app/src/main/java/com/vgkontact/app/GroupsActivity.kt
+++ app/src/main/java/com/vgkontact/app/GroupsActivity.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.HorizontalScrollView
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
