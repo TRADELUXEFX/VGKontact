5986c80a3bb2e565b4b7902a4a00f2e86f77e0f3
TRADELUXEFX <tradeluxefx@gmail.com>
2026-09-02 13:08:23 +0100

    Fix: Restore UpgradePlanActivity.kt with proper Kotlin code

--- app/src/main/java/com/vgkontact/app/UpgradePlanActivity.kt
+++ app/src/main/java/com/vgkontact/app/UpgradePlanActivity.kt
@@ -1,1 +1,11 @@
-<removed>
+package com.vgkontact.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UpgradePlanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upgrade_plan)
+    }
+}
