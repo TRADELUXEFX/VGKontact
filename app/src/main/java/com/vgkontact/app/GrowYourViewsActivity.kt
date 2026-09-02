c7c82d2f2b5855bf4b92e3a3bd18bc16d0351e4a
TRADELUXEFX <tradeluxefx@gmail.com>
2026-09-02 13:09:02 +0100

    Fix: Restore GrowYourViewsActivity.kt with proper Kotlin code

--- app/src/main/java/com/vgkontact/app/GrowYourViewsActivity.kt
+++ app/src/main/java/com/vgkontact/app/GrowYourViewsActivity.kt
@@ -1,1 +1,11 @@
-<removed>
+package com.vgkontact.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GrowYourViewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grow_your_views)
+    }
+}
