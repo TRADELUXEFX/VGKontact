18f48ca0c35880da3c9ff560c57d3a1de088e59e
TRADELUXEFX <tradeluxefx@gmail.com>
2026-09-03 04:00:54 +0100

    Update ProfileActivity.kt

--- app/src/main/java/com/vgkontact/app/ProfileActivity.kt
+++ app/src/main/java/com/vgkontact/app/ProfileActivity.kt
@@ -42,6 +42,7 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        FloatingContactHelper.attach(this)
         BottomNavHelper.setup(this, BottomNavHelper.Tab.PROFILE)
 
         window.statusBarColor = ContextCompat.getColor(this, R.color.vg_green)
