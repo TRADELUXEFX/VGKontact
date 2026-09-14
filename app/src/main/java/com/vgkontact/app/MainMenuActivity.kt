bf38ba57fc827956e100114e73232343bb6213e4
TRADELUXEFX <tradeluxefx@gmail.com>
2026-09-14 08:58:29 +0100

    Update MainMenuActivity.kt

--- app/src/main/java/com/vgkontact/app/MainMenuActivity.kt
+++ app/src/main/java/com/vgkontact/app/MainMenuActivity.kt
@@ -10,6 +10,7 @@ import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
